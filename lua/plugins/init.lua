6d61efb483e36ec0506e426266f1ef1b6ae0f4d0
Ehsan U <lancerf562@gmail.com>
2024-11-22 12:10:29 +0500

    add surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -71,6 +71,17 @@ return {
       require("Comment").setup()
     end
   },
+  
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
 
   {
     "stevearc/conform.nvim",
