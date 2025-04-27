97081afc321e2b70a738aa619f58847934191398
Ehsan U. <lancerf562@gmail.com>
2025-04-27 16:10:31 +0500

    snippets added:

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", ";", ":")
+vim.keymap.set("n", "<tab>", "<cmd>bnext<cr>", { desc = "next buf" })
+vim.keymap.set("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "prev buf" })
+vim.keymap.set("n", "x", function()
+  Snacks.bufdelete()
+end, { desc = "del buffer" })
+
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "move down" })
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "move up" })
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "move left" })
+vim.keymap.set("i", "<C-l>", "<Right>", { desc = "move right" })
