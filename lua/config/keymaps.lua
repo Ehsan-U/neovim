8e14732ca6593d3376366b49dd46c935bf40ac08
Ehsan U. <lancerf562@gmail.com>
2025-04-28 10:34:44 +0500

    Add custom keymaps and options for improved navigation and formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<tab>", "<cmd>bnext<cr>", { desc = "next buf" })
 vim.keymap.set("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "prev buf" })
@@ -10,6 +11,7 @@ end, { desc = "del buffer" })
 
 vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("i", "<C-j>", "<Down>", { desc = "move down" })
+--vim.keymap.del("i", "<C-k>") -- unset first (by default used by LSP)
 vim.keymap.set("i", "<C-k>", "<Up>", { desc = "move up" })
 vim.keymap.set("i", "<C-h>", "<Left>", { desc = "move left" })
 vim.keymap.set("i", "<C-l>", "<Right>", { desc = "move right" })
